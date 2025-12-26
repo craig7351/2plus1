5b196994dcf1b8bc1a3489b76709afc272b8c624
craig7351 <craig7351@hotmail.com>
2025-12-26 21:19:21 +0800

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/2plus1/',
   plugins: [react()],
   server: {
     host: true,  // 監聽所有網路介面，讓手機可以連線
